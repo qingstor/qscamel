7a19ca87ffc8ee66a7cfba3ef33df4da93945c15
Xuanwo <xuanwo@yunify.com>
2018-03-20 20:58:34 +0800

    utils: Add Join and Relative
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

---
+++ utils/path_test.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type joinCase struct {
+	in  []string
+	out string
+}
+
+func TestJoin(t *testing.T) {
+	joinTestCases := []joinCase{
+		{[]string{"/"}, ""},
+		{[]string{"//"}, ""},
+		{[]string{"//", "/"}, ""},
+
+		{[]string{"a"}, "a"},
+
+		{[]string{"a", "b"}, "a/b"},
+		{[]string{"a/", "b"}, "a/b"},
+		{[]string{"a/", "b/"}, "a/b"},
+	}
+
+	for _, v := range joinTestCases {
+		assert.Equal(t, v.out, Join(v.in...))
+	}
+}
+
+type relativeCase struct {
+	full   string
+	prefix string
+	out    string
+}
+
+func TestRelative(t *testing.T) {
+	relativeTestCases := []relativeCase{
+		{"/a/b/c", "/a/b", "c"},
+		{"/a/b/c", "/a", "b/c"},
+		{"/a/b/c", "a/", "b/c"},
+		{"/a/b/c", "a/b", "c"},
+		{"/a/b/c", "a/b/", "c"},
+		{"/a/b/c", "b/", "a/b/c"},
+	}
+
+	for _, v := range relativeTestCases {
+		assert.Equal(t, v.out, Relative(v.full, v.prefix))
+	}
+}
