c104b304107d7e1f8947a560fcde4410e41499cd
Xuanwo <xuanwo@yunify.com>
2018-03-25 22:16:52 +0800

    migrate: Check not finished object after returned
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- migrate/run.go
+++ migrate/run.go
@@ -7,33 +7,40 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/yunify/qscamel/constants"
+	"github.com/yunify/qscamel/model"
 )
 
 // Run will execute task run.
 func Run(ctx context.Context) (err error) {
 	switch t.Type {
 	case constants.TaskTypeCopy:
-		if !CanCopy() {
-			logrus.Infof("Source type %s and destination type %s not support copy.",
-				t.Src.Type, t.Dst.Type)
-			return
-		}
-		err = backoff.Retry(func() error { return Copy(ctx) }, backoff.NewExponentialBackOff())
+		err = copyTask(ctx)
 		if err != nil {
 			return
 		}
 	case constants.TaskTypeFetch:
-		if !CanFetch() {
-			logrus.Infof("Source type %s and destination type %s not support fetch.",
-				t.Src.Type, t.Dst.Type)
-			return
-		}
-		err = backoff.Retry(func() error { return Fetch(ctx) }, backoff.NewExponentialBackOff())
+		err = fetchTask(ctx)
 		if err != nil {
 			return
 		}
-	case constants.TaskTypeVerify:
-		return
+	case constants.TaskTypeVerifyCopy:
+		err = verifyTask(ctx)
+		if err != nil {
+			return
+		}
+		err = copyTask(ctx)
+		if err != nil {
+			return
+		}
+	case constants.TaskTypeVerifyFetch:
+		err = verifyTask(ctx)
+		if err != nil {
+			return
+		}
+		err = fetchTask(ctx)
+		if err != nil {
+			return
+		}
 	default:
 		logrus.Errorf("Task %s's type %s is not supported.", t.Name, t.Type)
 		return
@@ -48,4 +55,81 @@ func Run(ctx context.Context) (err error) {
 
 	logrus.Infof("Task %s has been finished.", t.Name)
 	return
+}
+
+func copyTask(ctx context.Context) (err error) {
+	if !CanCopy() {
+		logrus.Infof("Source type %s and destination type %s not support copy.",
+			t.Src.Type, t.Dst.Type)
+		return
+	}
+	logrus.Debugf("Start copy task.")
+	return backoff.Retry(func() error {
+		err := Copy(ctx)
+		if err != nil {
+			return err
+		}
+
+		ho, err := model.HasObject(ctx)
+		if err != nil {
+			logrus.Panic(err)
+		}
+		if ho {
+			logrus.Infof("There are not finished objects, retried.")
+			return constants.ErrNotFinishedObject
+		}
+
+		return nil
+	}, backoff.NewExponentialBackOff())
+}
+
+func fetchTask(ctx context.Context) (err error) {
+	if !CanFetch() {
+		logrus.Infof("Source type %s and destination type %s not support fetch.",
+			t.Src.Type, t.Dst.Type)
+		return
+	}
+	logrus.Debugf("Start fetch task.")
+	return backoff.Retry(func() error {
+		err := Fetch(ctx)
+		if err != nil {
+			return err
+		}
+
+		ho, err := model.HasObject(ctx)
+		if err != nil {
+			logrus.Panic(err)
+		}
+		if ho {
+			logrus.Infof("There are not finished objects, retried.")
+			return constants.ErrNotFinishedObject
+		}
+
+		return nil
+	}, backoff.NewExponentialBackOff())
+}
+
+func verifyTask(ctx context.Context) (err error) {
+	logrus.Debugf("Start verify task.")
+	return backoff.Retry(func() error {
+		err = Verify(ctx)
+		if err != nil {
+			return err
+		}
+
+		switch t.Type {
+		case constants.TaskTypeVerifyCopy:
+			t.Type = constants.TaskTypeCopy
+		case constants.TaskTypeVerifyFetch:
+			t.Type = constants.TaskTypeFetch
+		default:
+			logrus.Errorf("Task %s's type %s is not supported.", t.Name, t.Type)
+			return nil
+		}
+		err = t.Save(ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, backoff.NewExponentialBackOff())
 }
