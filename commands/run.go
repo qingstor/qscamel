743d7a2c1c76806ea36319276d122832b598f68c
Xuanwo <xuanwo@yunify.com>
2018-05-09 01:00:07 +0800

    commands: Print current version while starting
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- commands/run.go
+++ commands/run.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/yunify/qscamel/config"
+	"github.com/yunify/qscamel/constants"
 	"github.com/yunify/qscamel/contexts"
 	"github.com/yunify/qscamel/migrate"
 	"github.com/yunify/qscamel/model"
@@ -70,6 +71,7 @@ var RunCmd = &cobra.Command{
 		ctx = utils.NewTaskContext(ctx, t.Name)
 
 		// Start migrate.
+		logrus.Infof("Current version: %s.", constants.Version)
 		logrus.Infof("Task %s migrate started.", t.Name)
 
 		err = migrate.Execute(ctx)
