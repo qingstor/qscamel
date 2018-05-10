cad7bfef70e188a0dd41ae756fc8911892df05a2
Xuanwo <xuanwo@yunify.com>
2018-05-10 15:28:20 +0800

    endpoint: qingstor: Fix get zone failed while using non-standard port
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- endpoint/qingstor/utils.go
+++ endpoint/qingstor/utils.go
@@ -10,15 +10,20 @@ import (
 
 // GetZone determines the zone of bucket.
 func (c *Client) GetZone() (zone string, err error) {
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
 	url := fmt.Sprintf("%s://%s.%s:%d", c.Protocol, c.BucketName, c.Host, c.Port)
 
-	r, err := http.Head(url)
+	r, err := client.Head(url)
 	if err != nil {
 		logrus.Errorf("Get QingStor zone failed for %v.", err)
 		return
 	}
 
 	// Example URL: https://bucket.zone.qingstor.com
-	zone = strings.Split(r.Request.URL.String(), ".")[1]
+	zone = strings.Split(r.Header.Get("Location"), ".")[1]
 	return
 }
