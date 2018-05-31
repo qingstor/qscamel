a155369ae8d48df9380df75b04f30b88ba3b4fd3
Xuanwo <xuanwo@yunify.com>
2018-05-31 15:22:11 +0800

    endpoint: qingstor: Add sotrage class support
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- endpoint/qingstor/constants.go
+++ endpoint/qingstor/constants.go
@@ -5,3 +5,9 @@ const DirectoryContentType = "application/x-directory"
 
 //MaxListObjectsLimit is the max limit for list objects.
 const MaxListObjectsLimit = 1000
+
+// Constants for storage class.
+const (
+	StorageClassStandard   = "STANDARD"
+	StorageClassStandardIA = "STANDARD_IA"
+)
