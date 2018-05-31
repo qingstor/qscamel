fc7e435608ece9b00cf3d5a2e3bdf27f3eef64eb
Xuanwo <xuanwo@yunify.com>
2018-05-31 14:12:25 +0800

    migrate: Refactor whole package
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- migrate/fetch.go
+++ migrate/fetch.go
@@ -25,20 +25,25 @@ func CanFetch() bool {
 
 // Fetch will do fetch job between src and dst.
 func Fetch(ctx context.Context) (err error) {
-	oc = make(chan *model.Object)
+	oc = make(chan *model.Object, contexts.Config.Concurrency*2)
 	jc = make(chan *model.Job)
-	wg = new(sync.WaitGroup)
 
+	owg = &sync.WaitGroup{}
+	jwg = &sync.WaitGroup{}
+
+	// Wait for all object finished.
+	defer owg.Wait()
 	// Close channel for no more object.
 	defer close(oc)
 	// Close channel for no more job.
 	defer close(jc)
 	// Wait for all job finished.
-	defer wg.Wait()
+	defer jwg.Wait()
 
 	go listWorker(ctx)
 
 	for i := 0; i < contexts.Config.Concurrency; i++ {
+		owg.Add(1)
 		go migrateWorker(ctx)
 	}
 
@@ -52,23 +57,15 @@ func Fetch(ctx context.Context) (err error) {
 }
 
 // fetchObject will do a real fetch.
-func fetchObject(ctx context.Context, p string) (err error) {
-	defer wg.Done()
-
-	url, err := src.Reach(ctx, p)
+func fetchObject(ctx context.Context, o *model.Object) (err error) {
+	url, err := src.Reach(ctx, o.Key)
 	if err != nil {
-		logrus.Errorf("Src reach %s failed for %v.", p, err)
+		logrus.Errorf("Src reach %s failed for %v.", o.Key, err)
 		return err
 	}
-	err = dst.Fetch(ctx, p, url)
+	err = dst.Fetch(ctx, o.Key, url)
 	if err != nil {
-		logrus.Errorf("Dst fetch %s failed for %v.", p, err)
-		return err
-	}
-
-	err = model.DeleteObject(ctx, p)
-	if err != nil {
-		logrus.Panicf("DeleteObject failed for %v.", err)
+		logrus.Errorf("Dst fetch %s failed for %v.", o.Key, err)
 		return err
 	}
 	return
