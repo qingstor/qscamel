6eac5e8d7684c2df6058a8a5c05a72ece2e9f5f8
Xuanwo <xuanwo@yunify.com>
2018-05-31 14:07:49 +0800

    Use leveldb instead of boltdb
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- db/db.go
+++ db/db.go
@@ -1,15 +1,15 @@
 package db
 
 import (
-	"github.com/coreos/bbolt"
 	"github.com/sirupsen/logrus"
 
-	"github.com/yunify/qscamel/constants"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/storage"
 )
 
 // Database stores database connection.
 type Database struct {
-	*bolt.DB
+	*leveldb.DB
 }
 
 // DatabaseOptions stores database options.
@@ -20,38 +20,22 @@ type DatabaseOptions struct {
 // NewDB will create a new database connection.
 func NewDB(opt *DatabaseOptions) (d *Database, err error) {
 	// Set NoFreelistSync to true to import write performance.
-	client, err := bolt.Open(opt.Address, 0644, &bolt.Options{NoFreelistSync: true})
-	if err != nil {
-		logrus.Errorf("Open database failed for %v.", err)
+	client, err := leveldb.OpenFile(opt.Address, nil)
+	if err == nil {
+		d = &Database{DB: client}
+		logrus.Debugf("Connected to database %s", opt.Address)
 		return
 	}
 
-	d = &Database{DB: client}
-	logrus.Debugf("Connected to Bolt database %s", opt.Address)
-	return
-}
-
-// Init will init current database.
-func (db *Database) Init() (err error) {
-	tx, err := db.Begin(true)
-	if err != nil {
-		logrus.Errorf("Start writable transaction failed for %v.", err)
-		return
-	}
-	defer tx.Rollback()
-
-	// Create task list bucket.
-	_, err = tx.CreateBucketIfNotExists([]byte(constants.KeyTaskList))
-	if err != nil {
-		logrus.Errorf("Create task bucket failed for %v.", err)
-		return
+	if _, ok := err.(*storage.ErrCorrupted); ok {
+		logrus.Errorf("Open database failed for %v, recovering.", err)
+		client, err = leveldb.RecoverFile(opt.Address, nil)
+		if err != nil {
+			logrus.Errorf("Database is corrupted and recover failed for %v.", err)
+			return
+		}
 	}
 
-	err = tx.Commit()
-	if err != nil {
-		logrus.Errorf("DB commit failed for %v.", err)
-		return
-	}
-
+	logrus.Errorf("Open database failed for %v.", err)
 	return
 }
