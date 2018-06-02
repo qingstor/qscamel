5911526e73e8520fadbd1bf76f630c1c9d43effc
Xuanwo <xuanwo@yunify.com>
2018-06-02 14:41:56 +0800

    endpoint: qingstor: Implement multipart upload
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- endpoint/qingstor/constants.go
+++ endpoint/qingstor/constants.go
@@ -3,11 +3,23 @@ package qingstor
 // DirectoryContentType is the content type for qingstor directory.
 const DirectoryContentType = "application/x-directory"
 
-//MaxListObjectsLimit is the max limit for list objects.
+// MaxListObjectsLimit is the max limit for list objects.
 const MaxListObjectsLimit = 1000
 
 // Constants for storage class.
 const (
 	StorageClassStandard   = "STANDARD"
 	StorageClassStandardIA = "STANDARD_IA"
+)
+
+// Multipart related constants.
+// ref: https://docs.qingcloud.com/qingstor/api/object/multipart/index.html
+const (
+	// DefaultMultipartBoundarySize is the default multipart boundary size.
+	// 64 * 1024 * 1024 = 67108864 B = 64 MB
+	DefaultMultipartBoundarySize = 67108864
+	// MaxMultipartBoundarySize is the max multipart boundary size.
+	// Over this, put object will be reset by server.
+	// 5 * 1024 * 1024 * 1024 = 5368709120 B = 5 GB
+	MaxMultipartBoundarySize = 5368709120
 )
