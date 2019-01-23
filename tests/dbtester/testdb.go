cea523ce9a5c3b521cb5833dd9b20f1514e7b995
FAtwAER <wyattpeng@yunify.com>
2019-01-23 19:42:11 +0800

    tests : checker: Add some utils for test database

---
+++ tests/dbtester/testdb.go
@@ -0,0 +1,40 @@
+package dbtester
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/storage"
+)
+
+// NewDB will create a new database connection.
+func NewDB(opt *DatabaseOptions) (d *Database, err error) {
+	// Set NoFreelistSync to true to import write performance.
+	client, err := leveldb.OpenFile(opt.Address, nil)
+	if err == nil {
+		d = &Database{DB: client}
+		logrus.Debugf("Connected to database %s", opt.Address)
+		return
+	}
+
+	if _, ok := err.(*storage.ErrCorrupted); ok {
+		logrus.Errorf("Open database failed for %v, recovering.", err)
+		client, err = leveldb.RecoverFile(opt.Address, nil)
+		if err != nil {
+			logrus.Errorf("Database is corrupted and recover failed for %v.", err)
+			return
+		}
+	}
+
+	logrus.Errorf("Open database failed for %v.", err)
+	return
+}
+
+// Database stores database connection.
+type Database struct {
+	*leveldb.DB
+}
+
+// DatabaseOptions stores database options.
+type DatabaseOptions struct {
+	Address string
+}
