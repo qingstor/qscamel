129d56e950cbbdbb0a36871bbba1321425bbaa1e
fATwaer <wyattpeng@yunify.com>
2019-02-27 14:34:52 +0800

    tests: Fix testing race in default configuration problem

--- tests/integration/default_testing.go
+++ tests/integration/default_testing.go
@@ -8,7 +8,7 @@ func TestDefaultRunCopy() {
 	fileMap, clean := utils.PrepareDefaultTest()
 	defer clean(fileMap)
 	utils.Execute(fileMap, "run")
-	utils.CheckOutput(fileMap, "Task [a-z0-9]* has been finished", 1)
+	utils.CheckOutput(fileMap, "Task [a-z0-9]* has been finished", -1)
 	utils.CheckDBNoObject(fileMap)
 
 }
@@ -19,7 +19,7 @@ func TestDefaultDelete() {
 	utils.Execute(fileMap, "run")
 	fileMap["delname"] = fileMap["name"]
 	utils.Execute(fileMap, "delete")
-	utils.CheckOutput(fileMap, "Task [a-z0-9]* has been deleted", 1)
+	utils.CheckOutput(fileMap, "Task [a-z0-9]* has been deleted", -1)
 	utils.CheckDBNoObject(fileMap)
 
 }
@@ -30,7 +30,7 @@ func TestDefalutStatus() {
 	utils.Execute(fileMap, "run")
 	utils.Execute(fileMap, "status")
 	utils.CheckOutput(fileMap, "Show status started", 1)
-	utils.CheckOutput(fileMap, "There are [0-9]* tasks totally", 1)
+	utils.CheckOutput(fileMap, "There are [0-9]* tasks totally", -1)
 	utils.CheckDBNoObject(fileMap)
 }
 
@@ -40,6 +40,6 @@ func TestDefaultClean() {
 	utils.Execute(fileMap, "run")
 	utils.Execute(fileMap, "clean")
 	utils.CheckOutput(fileMap, "Clean started", 1)
-	utils.CheckOutput(fileMap, "Task [a-z0-9]* has been cleaned", 1)
+	utils.CheckOutput(fileMap, "Task [a-z0-9]* has been cleaned", -1)
 	utils.CheckDBNoObject(fileMap)
 }
