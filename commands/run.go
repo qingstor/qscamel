73003af4b533b515f6ee75ecbd5b3d8d4b49696c
Lance <281239768@qq.com>
2020-12-02 15:20:14 +0800

    commands: Refactor commands struct, extract common func (#298)

--- commands/run.go
+++ commands/run.go
@@ -5,16 +5,17 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/pengsrc/go-shared/pid"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
-	"github.com/yunify/qscamel/config"
 	"github.com/yunify/qscamel/constants"
-	"github.com/yunify/qscamel/contexts"
 	"github.com/yunify/qscamel/migrate"
 	"github.com/yunify/qscamel/model"
 	"github.com/yunify/qscamel/utils"
+)
+
+var (
+	taskPath string
 )
 
 // RunCmd will provide run command for qscamel.
@@ -22,40 +23,10 @@ var RunCmd = &cobra.Command{
 	Use:   "run [task name or task path]",
 	Short: "Create or resume a task",
 	Args:  cobra.ExactArgs(1),
+	PreRunE: func(cmd *cobra.Command, _ []string) error {
+		return initContext(cmd.Flag("config").Value.String())
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		c := &config.Config{}
-		c.LoadFromFilePath(cmd.Flag("config").Value.String())
-
-		// Check config.
-		err := c.Check()
-		if err != nil {
-			logrus.Errorf("Config check failed for %v.", err)
-			return
-		}
-
-		// Create PID file.
-		if pidfile := c.PIDFile; pidfile != "" {
-			p, err := pid.New(pidfile)
-			if err != nil {
-				logrus.Errorf("PID create failed for %v.", err)
-				return
-			}
-			defer func() {
-				err = p.Remove()
-				if err != nil {
-					logrus.Errorf("PID remove failed for %v.", err)
-				}
-			}()
-		}
-
-		// Setup contexts.
-		err = contexts.SetupContexts(c)
-		if err != nil {
-			logrus.Errorf("Contexts setup failed for %v.", err)
-			return
-		}
-		defer contexts.DB.Close()
-
 		ctx := context.Background()
 
 		sigs := make(chan os.Signal, 1)
@@ -64,8 +35,7 @@ var RunCmd = &cobra.Command{
 			sig := <-sigs
 			logrus.Infof("Signal %v received, exit for now.", sig)
 
-			contexts.DB.Close()
-
+			cleanUp()
 			os.Exit(0)
 		}()
 
@@ -92,12 +62,7 @@ var RunCmd = &cobra.Command{
 			logrus.Errorf("Migrate failed for %v.", err)
 		}
 	},
-}
-
-var (
-	taskPath string
-)
-
-func init() {
-	RunCmd.Flags().StringVarP(&taskPath, "task", "t", "", "task path")
+	PostRunE: func(cmd *cobra.Command, args []string) error {
+		return cleanUp()
+	},
 }
